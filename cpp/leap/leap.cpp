5ebecd22acb969bbc6210b148d523f346c3ea4a9
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-23 08:47:09 +0530

    imporve leap exercise

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -4,10 +4,14 @@ namespace leap
 {
     bool is_leap_year(int year)
     {
-        if (year % 2 == 1)
-            return false;
-        if (year % 100 == 0 && year % 400 != 0)
-            return false;
-        return year % 4 == 0;
+        // if (year % 4 == 1)
+        //     return false;
+        // if (year % 100 == 0 && year % 400 != 0)
+        //     return false;
+        // return year % 4 == 0;
+
+        // More efficient solution
+        // as the evaluation happens from the most likely to least likely
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
     }
 } // namespace leap
