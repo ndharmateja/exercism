ec745ab790eb2f409425dba579f6831fb5185c1d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-24 06:39:24 +0530

    solve reverse string

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,7 +1,16 @@
 #include "reverse_string.h"
+#include <utility>
 
-namespace reverse_string {
-
-// TODO: add your solution here
-
-}  // namespace reverse_string
+namespace reverse_string
+{
+    std::string reverse_string(std::string input)
+    {
+        if (input.size() <= 1)
+            return input;
+        int lo = 0;
+        int hi = input.size() - 1;
+        while (lo < hi)
+            std::swap(input[lo++], input[hi--]);
+        return input;
+    }
+} // namespace reverse_string
--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -1,7 +1,7 @@
 #pragma once
+#include <string>
 
-namespace reverse_string {
-
-// TODO: add your solution here
-
-}  // namespace reverse_string
+namespace reverse_string
+{
+    std::string reverse_string(std::string);
+} // namespace reverse_string
