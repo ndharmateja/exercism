61c11ab377d499e90f9bba7cd7a5b51a400497d3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-25 07:46:25 +0530

    solve allergies exercise

--- cpp/allergies/allergies.cpp
+++ cpp/allergies/allergies.cpp
@@ -1,7 +1,31 @@
 #include "allergies.h"
+#include <array>
 
-namespace allergies {
+namespace allergies
+{
+    // const char * is faster than string
+    // strings are created in read only memory
+    static const std::array<const char *, 8> allergens_list = {
+        "eggs", "peanuts", "shellfish", "strawberries",
+        "tomatoes", "chocolate", "pollen", "cats"};
 
-// TODO: add your solution here
+    // Constructor
+    allergy_test::allergy_test(const int &score)
+    {
+        // We only need to check the lower 8 bits of score
+        for (size_t i = 0; i < allergens_list.size(); i++)
+        {
+            // If the ith bit from the right is set, we add that allergen
+            if (score & 1 << i)
+                allergens.emplace(allergens_list[i]);
+        }
+    }
 
-}  // namespace allergies
+    // Member functions
+    // const as these fns don't change state
+    // If count is positive, then bool returned is true
+    // and viceversa
+    bool allergy_test::is_allergic_to(const std::string &allergen) const { return allergens.count(allergen); }
+
+    std::unordered_set<std::string> allergy_test::get_allergies() const { return allergens; }
+} // namespace allergies
--- cpp/allergies/allergies.h
+++ cpp/allergies/allergies.h
@@ -1,7 +1,21 @@
 #pragma once
+#include <string>
+#include <unordered_set>
 
-namespace allergies {
+namespace allergies
+{
+    class allergy_test
+    {
+    private:
+        std::unordered_set<std::string> allergens;
 
-// TODO: add your solution here
+    public:
+        // Constructor
+        allergy_test(const int &score);
 
-}  // namespace allergies
+        // Member functions
+        // const as these fns don't change state
+        bool is_allergic_to(const std::string &) const;
+        std::unordered_set<std::string> get_allergies() const;
+    };
+} // namespace allergies
