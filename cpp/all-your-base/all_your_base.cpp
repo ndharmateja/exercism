fa24d510e86fb40a0c0ced7937c79a01fec70277
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-31 05:04:12 +0530

    c++: add comment explanation for all your base

--- cpp/all-your-base/all_your_base.cpp
+++ cpp/all-your-base/all_your_base.cpp
@@ -6,6 +6,12 @@ namespace all_your_base
 {
     unsigned convert_to_decimal(std::vector<unsigned> digits, unsigned base)
     {
+        // ax^3 + bx^2 + cx^1 + d = (((0x + a)x + b)x + c)x + d
+        // result = 0
+        // result = result * x + a = 0x + a = a
+        // result = result * x + b = (0x + a)x + b = ax + b
+        // result = result * x + c = (0x + a)x + b)x + c = ax^2 + bx + c
+        // result = result * x + d = (0x + a)x + b)x + c)x + d = ax^3 + bx^2 + cx + d
         unsigned result = 0;
         for (const auto digit : digits)
         {
