a2bc27c14e67207a7a6b6519709c6c13cdd47eef
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-30 17:01:45 +0530

    c++: solve binary search

---
+++ cpp/binary-search/CMakeFiles/binary-search.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binary-search.dir/binary_search.cpp.o"
+  "CMakeFiles/binary-search.dir/binary_search.cpp.o.d"
+  "CMakeFiles/binary-search.dir/binary_search_test.cpp.o"
+  "CMakeFiles/binary-search.dir/binary_search_test.cpp.o.d"
+  "CMakeFiles/binary-search.dir/test/tests-main.cpp.o"
+  "CMakeFiles/binary-search.dir/test/tests-main.cpp.o.d"
+  "binary-search"
+  "binary-search.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binary-search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
