897fe31185ae7b2e94eadb923d71a30b13810e4f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-30 18:49:02 +0530

    c++: solve complex numbers

---
+++ cpp/complex-numbers/CMakeFiles/complex-numbers.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dharma/projects/exercism/cpp/complex-numbers/complex_numbers.cpp" "CMakeFiles/complex-numbers.dir/complex_numbers.cpp.o" "gcc" "CMakeFiles/complex-numbers.dir/complex_numbers.cpp.o.d"
+  "/Users/dharma/projects/exercism/cpp/complex-numbers/complex_numbers_test.cpp" "CMakeFiles/complex-numbers.dir/complex_numbers_test.cpp.o" "gcc" "CMakeFiles/complex-numbers.dir/complex_numbers_test.cpp.o.d"
+  "/Users/dharma/projects/exercism/cpp/complex-numbers/test/catch_amalgamated.cpp" "CMakeFiles/complex-numbers.dir/test/catch_amalgamated.cpp.o" "gcc" "CMakeFiles/complex-numbers.dir/test/catch_amalgamated.cpp.o.d"
+  "/Users/dharma/projects/exercism/cpp/complex-numbers/test/tests-main.cpp" "CMakeFiles/complex-numbers.dir/test/tests-main.cpp.o" "gcc" "CMakeFiles/complex-numbers.dir/test/tests-main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
