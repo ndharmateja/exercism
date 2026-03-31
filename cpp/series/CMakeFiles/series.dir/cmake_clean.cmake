fe8619921942543bb62d0bdaed700d75e826ea3b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-31 15:20:14 +0530

    c++: solve series exercise

---
+++ cpp/series/CMakeFiles/series.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/series.dir/series.cpp.o"
+  "CMakeFiles/series.dir/series.cpp.o.d"
+  "CMakeFiles/series.dir/series_test.cpp.o"
+  "CMakeFiles/series.dir/series_test.cpp.o.d"
+  "CMakeFiles/series.dir/test/tests-main.cpp.o"
+  "CMakeFiles/series.dir/test/tests-main.cpp.o.d"
+  "series"
+  "series.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/series.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/series/CMakeFiles/test_series.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_series"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/test_series.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
