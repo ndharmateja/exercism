d0403ae212bed75eb18693d79701c7d23a7488ec
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-03-31 16:02:22 +0530

    c++: improve grains exercise

--- cpp/grains/grains.cpp
+++ cpp/grains/grains.cpp
@@ -9,5 +9,6 @@ namespace grains
     // Total grains = 2^0 + 2^1 + ... + 2^63 = 2^64 - 1
     // In a 64 bit number, the bit pattern is 11111....11 (64 bits)
     // we can get that by subtracting 1 from 0ULL
-    unsigned long long total() { return 0ULL - 1; }
+    // or by flipping all bits of 0ULL
+    unsigned long long total() { return ~0ULL; }
 } // namespace grains
